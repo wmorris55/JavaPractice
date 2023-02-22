64d80b94de6563d9346a10131b4dfcd522b7f16a
Will Morris <willm055@yahoo.com>
2023-02-22 08:03:19 -0500

    java test 2/22/23

---
+++ TenHellos2.java
@@ -0,0 +1,13 @@
+public class TenHellos2
+{
+	public static void main(String[] args)
+	{//Print 10 Hellos
+		int i = 1;
+		while (i <= 10)
+		{
+			System.out.println(i + "th Hello");
+			i = i + 1;
+		}
+
+	}
+}
