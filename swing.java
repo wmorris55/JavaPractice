be81ffc917dfe60ce17d9b592e912f133e506b05
Will Morris <willm055@yahoo.com>
2023-03-05 19:29:19 -0500

    try again

---
+++ swing.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class swing {
+
+	public static void main(String[] args) {
+		
+		String name = JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null, "Hello " + name);
+		
+		
+	}
+
+}
